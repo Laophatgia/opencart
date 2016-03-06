9de1b9974443239aa5978d04bd1e523dd0a34db9
Laophatgia <hieuntse02172@gmail.com>
2016-03-06 09:28:51 +0700

    update configure
    update configure

--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/exotric/');
+define('HTTP_SERVER', 'http://laophatgia.esy.es/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/exotric/');
+define('HTTPS_SERVER', 'http://laophatgia.esy.es/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/exotric/catalog/');
-define('DIR_SYSTEM', 'C:/xampp/htdocs/exotric/system/');
-define('DIR_LANGUAGE', 'C:/xampp/htdocs/exotric/catalog/language/');
-define('DIR_TEMPLATE', 'C:/xampp/htdocs/exotric/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/xampp/htdocs/exotric/system/config/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/exotric/image/');
-define('DIR_CACHE', 'C:/xampp/htdocs/exotric/system/cache/');
-define('DIR_DOWNLOAD', 'C:/xampp/htdocs/exotric/system/download/');
-define('DIR_UPLOAD', 'C:/xampp/htdocs/exotric/system/upload/');
-define('DIR_MODIFICATION', 'C:/xampp/htdocs/exotric/system/modification/');
-define('DIR_LOGS', 'C:/xampp/htdocs/exotric/system/logs/');
+define('DIR_APPLICATION', '/home/u156851369/public_html/catalog/');
+define('DIR_SYSTEM', '/home/u156851369/public_html/system/');
+define('DIR_LANGUAGE', '/home/u156851369/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/u156851369/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/u156851369/public_html/system/config/');
+define('DIR_IMAGE', '/home/u156851369/public_html/image/');
+define('DIR_CACHE', '/home/u156851369/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/u156851369/public_html/system/download/');
+define('DIR_UPLOAD', '/home/u156851369/public_html/system/upload/');
+define('DIR_MODIFICATION', '/home/u156851369/public_html/system/modification/');
+define('DIR_LOGS', '/home/u156851369/public_html/system/logs/');
 
 // DB
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'exotric');
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'mysql.hostinger.vn');
+define('DB_USERNAME', 'u156851369_sam');
+define('DB_PASSWORD', 'samsam');
+define('DB_DATABASE', 'u156851369_sam');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
